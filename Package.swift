4c5b48424b0d58a939cc3c5bb2849c792db12fd0
x0y14 <ya01ma08@gmail.com>
2025-03-27 00:44:02 +0900

    依存の追加

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
             name: "brevsenproto",
             targets: ["brevsenproto"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/connectrpc/connect-swift", "1.0.2"..<"2.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "brevsenproto"),
+            name: "brevsenproto",
+            dependencies: [
+                .product(name: "Connect", package: "connect-swift")
+            ]
+        ),
         .testTarget(
             name: "brevsenprotoTests",
             dependencies: ["brevsenproto"]
